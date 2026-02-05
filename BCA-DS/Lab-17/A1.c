82963448274912c9c5f7c2bf670e4bfd8d21aa5b
Tech Avengers <chinkiminki345@gmail.com>
2026-02-05 05:22:41 +0000

    delete in kinked list

---
+++ BCA-DS/Lab-17/A1.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int size = 0;
+
+struct node {
+    int info;
+    struct node *link;
+};
+
+struct node *first = NULL;
+
+void insertAtFirst(int data) {
+    struct node *avail, *newNode;
+    avail = (struct node *)malloc(sizeof(struct node));
+
+    if (avail == NULL) {
+        printf("Unable to allocate memory.");
+        return;
+    }
+
+    newNode = avail;
+    avail = avail->link;
+
+    newNode->info = data;
+
+    if (first == NULL) {
+        newNode->link = NULL;
+    }
+
+    else {
+        newNode->link = first;
+    }
+
+    first = newNode;
+    size++;
+    printf("DATA INSERTED SUCCESSFULLY\n");
+}
+
+void deleteFirst() {
+
+    if(first==NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    struct node *temp = first;
+    first = first->link;
+
+    free(temp);
+}
+
+void deleteLast() {
+    if(first==NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    if(first->link==NULL) {
+        struct node *temp = first;
+        first=NULL;
+        free(temp);
+    }
+
+    struct node *ptr = first;
+    while((ptr->link)->link!=NULL) {
+        ptr = ptr->link;
+    }
+
+    struct node* temp = ptr->link;
+    ptr->link=NULL;
+    free(temp);
+}
+
+void display() {
+
+    if (first == NULL) {
+        printf("List is empty.");
+        return;
+    }
+    struct node *ptr = first;
+
+    printf("\n --------- Data in Linked list --------- \n");
+    while (ptr != NULL) {
+        printf("%d ->", ptr->info);
+        ptr = ptr->link;
+    }
+
+    printf("\n------ TOTAL NODES: %d -------\n ", size);
+    printf("\n ----------------------- \n");
+}
+
+void insertAtEnd(int value) {
+    struct node *avail, *newNode;
+    avail = (struct node *)malloc(sizeof(struct node));
+
+    if (avail == NULL) {
+        printf("\nUnable to allocate memory.");
+        return;
+    }
+
+    newNode = avail;
+    avail = avail->link;
+
+    newNode->info = value;
+    newNode->link = NULL;
+
+    if (first == NULL) {
+        first = newNode;
+        printf("\nNode inserted successfully!!");
+        return;
+    }
+    
+    struct node *ptr = first;
+    while (ptr->link != NULL) {
+        ptr = ptr->link;
+    }
+    ptr->link = newNode;
+
+    printf("\nNode inserted successfully!!");
+
+    size++;
+}
+
+int main() {
+    int i, choice = 5, value, loc;
+    while (choice != 0) {
+        printf("\n Enter 1 for Insert at Beginning");
+        printf("\n Enter 2 for Insert at End");
+        printf("\n Enter 3 for Delete from First");
+        printf("\n Enter 4 for Delete from Last");
+        printf("\n Enter 5 for Display");
+        printf("\n Enter -1 for Exit");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("ENter value");
+                scanf("%d", &value);
+                insertAtFirst(value);
+                break;
+            case 2:
+                printf("ENter value");
+                scanf("%d", &value);
+                insertAtEnd(value);
+                break;
+            case 3:
+                deleteFirst();
+                break;
+            case 4:
+                deleteLast();
+                break;
+            case 5:
+                display();
+                break;
+            default:
+                printf("You entered wronge choice!!!\n");
+        }
+        display();
+    }
+    return 0;
+}
